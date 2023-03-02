f91fc3b2d9c7a8908600d0fd988314b53ec0e742
alsiheev <alexsiheev@gmail.com>
2023-03-02 13:26:00 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import styled from "styled-components";
-import BlogCard from "./components/BlogCard/BlogCard";
-
 function App() {
-  return (
-    <ContainerTest>
-      <BlogCard />
-    </ContainerTest>
-  );
+  return <div></div>;
 }
-
-const ContainerTest = styled.div``;
 
 export default App;
