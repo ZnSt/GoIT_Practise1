d54023579cb3ef210259ee8ebc386e094d57e803
AlinaS13 <117362222+AlinaS13@users.noreply.github.com>
2023-03-03 14:49:48 +0200

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import transactions from "./data/transaction.json";
+import TransactionHistory from "./component/transactions/transactionsHistory";
 const App = () => {
-  return <div>I am App component</div>;
+  return (
+    <div>
+      <TransactionHistory items={transactions} />;
+    </div>
+  );
 };
 
 export default App;
