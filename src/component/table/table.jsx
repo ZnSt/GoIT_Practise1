070950d7282ca07307be14911326cc6930f08315
makoviisergii <sergiimakovii78@gmail.com>
2023-03-03 16:48:24 +0200

    end

--- src/component/table/table.jsx
+++ src/component/table/table.jsx
@@ -1,5 +1,14 @@
+import styled  from "styled-components";
+
 const Table = ({ children }) => {
-  return <table>{children}</table>;
+  return <TableContainer>{children}</TableContainer>;
 };
 
 export default Table;
+
+const TableContainer= styled.table`
+  margin: 40px;
+  /* border: 1px solid gray; */
+  border-collapse: collapse;
+
+` 
