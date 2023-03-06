556b804afda16796767f00ae03af5f81bd43a6fb
OlexandrSi <oleksandr.si@alphax.digital>
2023-03-06 18:49:52 +0200

    Init day 2

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-WEEK 2 - DAY 1
+WEEK 2 - DAY 2
 
 TASK: https://github.com/josserden/react-blended/blob/lesson-02/source-code/task%231.md
+
+1. Debounce
+2. Console Ninja extension
+3. Grid with placeholder data
+4. Api
+5. Styling
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -6,24 +6,26 @@ export class SearchForm extends Component {
     value: "",
   };
 
-  componentDidMount() {
-    console.log("Я народився");
-  }
-
   componentDidUpdate() {
-    console.log("Я еволюціонував");
-  }
+    if (this.timeout !== null) {
+      clearTimeout(this.timeout);
+    }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state.value);
-  };
+    this.timeout = setTimeout(() => {
+      console.log(this.state.value);
+    }, 200);
+  }
 
   handleChange = (event) => {
     this.setState({
       value: event.target.value,
     });
   };
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(this.state.value);
+  // };
 
   render() {
     const { value } = this.state;
