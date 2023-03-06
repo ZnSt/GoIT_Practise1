1d0c71d69cfc3858895aa4918082ffeffb70b223
OlexandrSi <oleksandr.si@alphax.digital>
2023-03-06 13:56:24 +0200

    absolute imports test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Test from "@/Test";
+import png_test from "media/png_test.png";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Test />
+      <img src={png_test} />
+    </div>
+  );
 };
 
 export default App;
