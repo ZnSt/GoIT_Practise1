b3272036aa2a9537d5e30615f1139569affdf774
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2023-03-13 15:16:36 +0200

    qwe

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,57 @@
-import { SearchForm } from "@/SearchForm";
-import { HeaderComponent } from "@/Header";
-import { ImageGrid } from "@/ImageGrid";
-import { fetchPhotos } from "api/api";
-import { Component, useEffect, useState } from "react";
-import Modal from "@/Modal/Modal";
+import { SearchForm } from "@/SearchForm"
+import { HeaderComponent } from "@/Header"
+import { ImageGrid } from "@/ImageGrid"
+import { fetchPhotos } from "api/api"
+import { Component, useEffect, useState } from "react"
+import Modal from "@/Modal/Modal"
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const [prevQuery, setPrevQuery] = useState("")
+  const [curPage, setCurPage] = useState(1)
+  const [photos, setPhotos] = useState(second)
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    setQuery(event.target.elements.searchfield.value);
-    setIsLoading(true);
-  };
+    event.preventDefault()
+    setQuery(event.target.elements.searchfield.value)
+    setIsLoading(true)
+  }
 
   useEffect(() => {
-    console.log(query, isLoading);
-  }, [isLoading]);
+    // componentDidUpdate check isLoading
+    const getPhotos = async (page) => {
+      const photos = await fetchPhotos(query, page)
+      setIsLoading(false)
+      console.log(photos)
+    }
+    if (isLoading) {
+      if (query !== prevQuery) {
+        getPhotos(curPage)
+        setPrevQuery(query)
+
+        console.log("if ")
+      } else {
+        getPhotos(curPage)
+        console.log("else, ")
+      }
+    }
+  }, [isLoading])
 
   return (
     <div>
       <HeaderComponent>
         <SearchForm onSubmit={handleSubmit} />
       </HeaderComponent>
-      {/* <ImageGrid
+      <ImageGrid
         images={photos}
         isLoading={isLoading}
-        openModal={this.toggleModal}
-        getPicture={this.openPicture}
-      /> */}
+        // openModal={this.toggleModal}
+        // getPicture={this.openPicture}
+      />
       {/* {showModal && <Modal src={modalImage} />} */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
