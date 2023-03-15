24796bac64e97b3f274d1e6f1b488c21054f89ce
VickyBondarenko <quikyv@gmail.com>
2023-03-15 14:46:56 +0100

    Add wrapper

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import Navigaion from "./components/Navigation";
+
 import Products from "./components/Products";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
+import NotFound from "./components/NotFound";
 import "./App.css";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<Wrapper />}>
+          <Route path="/products" element={<Products />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,31 @@
-
-
-
+import { NavLink } from "react-router-dom";
 
 import styled from "styled-components";
 
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
 
 export default function Navigaion() {
-
   const logout = () => {};
 
   return (
     <>
       <div className="navigation">
-
+        <StyledLink className="nav__item" to="/products">
+          Products
+        </StyledLink>
+        <StyledLink className="nav__item mla" to="/login">
+          Login
+        </StyledLink>
+        <StyledLink className="nav__item" to="/registration">
+          Registration
+        </StyledLink>
       </div>
-
     </>
   );
 }
