215d9be1409c75f862b16229681bfd5304c3801c
marvall (Yaroslav Bezugliy) <marvallua@gmail.com>
2023-03-21 13:48:48 +0200

    init

--- src/context.js
+++ src/context.js
@@ -2,13 +2,6 @@ import { createContext, useContext, useState } from "react";
 
 const AppState = createContext();
 
-export const CustomProvider = ({ children, value }) => {
-  const [contextState, setContextState] = useState(value);
-  return (
-    <AppState.Provider value={{ ...contextState, setContextState }}>
-      {children}
-    </AppState.Provider>
-  );
-};
+c
 
 export const useAppState = () => useContext(AppState);
